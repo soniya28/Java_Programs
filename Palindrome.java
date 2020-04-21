068430a7d16911941218a28d0d336f8a9eb32839
soniya28 <58620015+soniya28@users.noreply.github.com>
2020-04-21 03:15:30 -0700

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -11,18 +11,18 @@ class Palindrome
 		int count=0;
 		for(int i=0,j=size-1;i<=(int)size/2||j>=(int)size/2;i++,j--)
 		{
-		if(string.charAt(i)==string.charAt(j))
-		{
-		count++;
-		}
+			if(string.charAt(i)==string.charAt(j))
+			{
+				count++;
+			}
 		}
 		if(count==(size/2)+1)
 		{
-		System.out.println("String is palindrome!!");
+			System.out.println("String is palindrome!!");
 		}
 		else
 		{
-		System.out.println("String is not palindrome!!");
+			System.out.println("String is not palindrome!!");
 		}
 	}
 }
