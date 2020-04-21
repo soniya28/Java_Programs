2eb86c6da13e53c3778bc0063ba29500b9b9c961
soniya28 <58620015+soniya28@users.noreply.github.com>
2020-04-21 03:22:19 -0700

    Update Int_pallindrome.java

--- Int_pallindrome.java
+++ Int_pallindrome.java
@@ -4,32 +4,32 @@ class Int_pallindrome
 {
 	public static void main(String[] args)
 	{
-	Scanner input=new Scanner(System.in);
-	System.out.println("Enter the value:");	
-	int num=input.nextInt();
-	int[] reverse=new int[20];
-	int i=1,k,j=0;
-	while(num!=0)
-	{
-	reverse[j]=num%10;
-	num=num/10;
-	j++;
-	}
-	int size=j,count=0;
-	for(i=0,k=size-1;i<=size/2|k>=size-1;i++,k--)
-	{
-	if(reverse[i]==reverse[k])
-	{
-	count++;
-	}
-	}
-	if(count==size/2+1)
-	{
-	System.out.println("Integer string is pallindrome!!");
-	}
-	else
-	{
-	System.out.println("Integer string is not a pallindrome!!");	
-	}
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the value:");	
+		int num=input.nextInt();
+		int[] reverse=new int[20];
+		int i=1,k,j=0;
+		while(num!=0)
+		{
+			reverse[j]=num%10;
+			num=num/10;
+			j++;
+		}
+		int size=j,count=0;
+		for(i=0,k=size-1;i<=size/2|k>=size-1;i++,k--)
+		{
+			if(reverse[i]==reverse[k])
+			{
+			count++;
+			}
+		}
+		if(count==size/2+1)
+		{
+			System.out.println("Integer string is pallindrome!!");
+		}
+		else
+		{	
+			System.out.println("Integer string is not a pallindrome!!");	
+		}
 	}
 }
